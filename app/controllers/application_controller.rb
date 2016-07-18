2d9f26c876689d6cebc74d56db2aa7463884cc2c
CAGG <genesis62@hotmail.com>
2016-07-18 08:48:58 -0600

    changes_computer_migration_include_macbluetooth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action 	:ensure_login
   helper_method 	:logged_in?, :current_user
-
+  
   protected
   def ensure_login
    	redirect_to login_path unless session[:user_id]
--- db/migrate/20160613161946_create_computers.rb
+++ db/migrate/20160613161946_create_computers.rb
@@ -1,33 +1,33 @@
 class CreateComputers < ActiveRecord::Migration
   def change
     create_table :computers do |t|
-      t.string :ninventary
-      t.string :nserie
-      t.string :brand
-      t.string :model
-      t.string :nfactura
-      t.date :buy_date
-      t.string :genus
-      t.string :processor
-      t.string :hd
-      t.string :memory
+      t.string  :ninventary
+      t.string  :nserie
+      t.string  :brand
+      t.string  :model
+      t.string  :nfactura
+      t.date    :buy_date
+      t.string  :genus
+      t.string  :processor
+      t.string  :hd
+      t.string  :memory
       t.boolean :bluetooth
       t.string  :macbluetooth
-      t.string :os
-      t.string :voffice
-      t.string :users
-      t.string :name
-      t.string :workgroup
+      t.string  :os
+      t.string  :voffice
+      t.string  :users
+      t.string  :name
+      t.string  :workgroup
       t.boolean :wifi
-      t.string :maclan
-      t.string :iplan
-      t.string :masklan
-      t.string :macwifi
-      t.string :ipwifi
-      t.string :maskwifi
+      t.string  :maclan
+      t.string  :iplan
+      t.string  :masklan
+      t.string  :macwifi
+      t.string  :ipwifi
+      t.string  :maskwifi
       t.boolean :operational
-      t.text :reazon
-      t.text :notes
+      t.text    :reazon
+      t.text    :notes
       t.references :Network, index: true, foreign_key: true
       t.references :Dependency, index: true, foreign_key: true
       t.references :Worker, index: true, foreign_key: true
