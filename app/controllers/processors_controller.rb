d103b0533b36440a4978e12e6ef9fa304cc5a52f
CAGG <genesis62@hotmail.com>
2016-08-03 08:57:20 -0600

    boot2table2all

--- app/controllers/processors_controller.rb
+++ app/controllers/processors_controller.rb
@@ -4,7 +4,7 @@ class ProcessorsController < ApplicationController
   # GET /processors
   # GET /processors.json
   def index
-    @processors = Processor.all
+    @processors = Processor.all.paginate(page: params[:page], per_page: 15 )
   end
 
   # GET /processors/1
