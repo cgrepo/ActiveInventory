247419b22c309934910afad8db01072dc38dd470
CAGG <genesis62@hotmail.com>
2016-08-08 10:26:35 -0600

    mixin_XsmallxMedium_layout-dependecy-worker

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -4,7 +4,7 @@ class WorkersController < ApplicationController
   # GET /workers
   # GET /workers.json
   def index
-    @workers = Worker.all.order(:name).paginate(page: params[:page], per_page: 15 )
+    @workers = Worker.all.order(:name).paginate(page: params[:page], per_page: 10 )
   end
 
   # GET /workers/1
