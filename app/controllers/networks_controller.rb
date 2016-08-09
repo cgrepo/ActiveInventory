7097773c1e23e667580f0f61aa9039e5e11932db
CAGG <genesis62@hotmail.com>
2016-08-08 21:57:00 -0600

    mix-net_pc_monitors

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -4,7 +4,7 @@ class NetworksController < ApplicationController
   # GET /networks
   # GET /networks.json
   def index
-    @networks = Network.all.paginate(page: params[:page], per_page: 15 )
+    @networks = Network.all.paginate(page: params[:page], per_page: 10 )
   end
 
   # GET /networks/1
