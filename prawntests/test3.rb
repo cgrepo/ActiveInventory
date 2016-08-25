2d24683bb925d5074834d4f4894caca7117c9c2c
CAGG <genesis62@hotmail.com>
2016-08-25 14:37:04 -0600

    updated-pdf-rendering80percent

--- prawntests/test3.rb
+++ prawntests/test3.rb
@@ -1,7 +1,6 @@
 require 'prawn'
 require 'date'
 
-#pdf = Prawn::Document.new
 Prawn::Document.generate 'theyard.pdf' do 
 	stroke_axis
 	Prawn::Font::AFM.hide_m17n_warning = true
@@ -12,96 +11,140 @@ Prawn::Document.generate 'theyard.pdf' do
 	image imgdir, at: [215,710], fit:[302,302]
 	font_families.update('Calibri' => { normal:'Calibri.ttf'  })
 	font_families.update('Verdana' => { normal:'Verdana.ttf'  })
- 	formatted_text_box [ 
-    	{:text => "SOLICITUD DE SERVICIO" , size:24, style:[:bold], font:'Verdana', color:'747373'  },
-     ], at:[130,650], width:420, height:300
+ 	formatted_text_box [
+    {:text => "SOLICITUD DE SERVICIO" , size:24, style:[:bold], font:'Verdana', color:'747373'  }, ], 
+    at:[130,650], width:420, height:300
 
-  formatted_text_box [ 
-    	{:text => "PRIORIDAD: " , size:11, style:[:normal], font:'Calibri' },
-    	{:text => " URGENTE-ATENDER LA SIGUIENTE SOLICITUD DE MANERA INMEDIATA!" , size:8, style:[:normal],
-    		font:'Calibri', color:'f90101' },
-    	{:text => " FECHA: " , size:9, style:[:normal], font:'Verdana', color:'a5a0a0' },
-    	{:text => "#{Time.now.strftime("%m/%d/%Y")} " , size:9, style:[:bold], font:'Helvetica' },
-    	{:text => " FOLIO: " , size:9, style:[:normal], font:'Calibri', color:'a5a0a0' },
-    	{:text => " 00000000001" , size:9, style:[:bold], font:'Verdana' }
-     ], at:[5,620]
-  formatted_text_box [ 
-    	{:text => 'Por medio de la presente se solicita el servicio a el equipo especificado en este documento a la empresa:' , 
-    		size:11, style:[:normal], font:'Calibri' },
-     ], at:[10,605]
+  formatted_text_box [
+    {:text => "PRIORIDAD: " , size:11, style:[:normal], font:'Calibri' },
+    {:text => " URGENTE-ATENDER LA SIGUIENTE SOLICITUD DE MANERA INMEDIATA!" , size:8, style:[:normal],
+    	font:'Calibri', color:'f90101' },
+    {:text => " FECHA: " , size:9, style:[:normal], font:'Verdana', color:'b0a8a8' },
+    {:text => "#{Time.now.strftime("%m/%d/%Y")} " , size:9, style:[:bold], font:'Helvetica' },
+    {:text => " FOLIO: " , size:9, style:[:normal], font:'Calibri', color:'b0a8a8' },
+    {:text => " 00000000001" , size:9, style:[:bold], font:'Verdana' }
+  ], at:[5,620]
+  formatted_text_box [
+    {:text => 'Por medio de la presente se solicita el servicio a el equipo especificado en este documento a la empresa:' , size:11, 
+      style:[:normal], font:'Calibri' },
+  ], at:[10,605]
 
-  
-  bounding_box([5,595], width:550, height:53 ) do
-  	move_down 6
-  	indent 5 do
-  		#stroke_axis(:step_length => 50)
-  		text 'Nombre Empresa: COPY SERVICIOS BAJA', size:10, font:'Helvetica'
-  		move_down 4
-  		text 'Direccion : Avenida Niños Heroes S/N. Centro.. Cabo San Lucas, Baja California Sur.', size:10, font:'Helvetica'
-  		move_down 4
-  		text 'Telefono(s): (624)144-4725 (624)144-4725 RFC: OEII789927H78 Email: carlos_albinoggat@hotmail.com', size:10, font:'Helvetica'
-  	end
-  	transparent(0.7) { stroke_bounds}
-  end
-  line [85,580],[550,580]
-  line [55, 564],[550,564]
-  line [60, 547],[185,547]
-  line [205,547],[270,547]
-  line [298,547],[550,547]
-	stroke_color 'c5c1c1'#'a5a0a0'
+	stroke_color 'c5c1c1'#'b0a8a8'
   stroke do
-
-  	horizontal_line 78,550, at:525
-  	horizontal_line 70,550, at:510
-  	horizontal_line 57,550, at:494
-  	rounded_rectangle [5,540], 550,70, 6
-
+    #-------FIRST BOX-------------------
+    rounded_rectangle [5,595], 550,50, 6
+    horizontal_line 10,550, at:580
+    horizontal_line 10,550, at:564
+    #-------SECOND----------------------
+    rounded_rectangle [5,540], 550,63, 6
+    horizontal_line 10,550, at:525
+    horizontal_line 10,550, at:510
+    horizontal_line 10,550, at:495
+    #----THIRD--------------------------
+    rounded_rectangle [5,472], 550,100, 6
+    horizontal_line 10,550, at:442
+    horizontal_line 10,550, at:428
+    #------FOURT------------------------
+    rounded_rectangle [5,367], 550,65, 6
+    horizontal_line 103,550, at:350
+    horizontal_line 10,550, at:337
+    horizontal_line 10,550, at:324
+    #horizontal_line 10,550, at:311
+    #------FIFTH------------------------
+    #rounded_rectangle [5,312], 550,120, 6
   end
+#--------------FIRST BOX DATA--------------------------------------------------------------------------------------------------------
+  formatted_text_box [
+    {text:'Nombre empresa: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' BAJA DIGITAL S.A DE C.V. ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'}
+  ], at:[10,590]
+  formatted_text_box [
+    {text:' Dirección: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' AVE. IGNACIO ALLENDE #880, LOCAL 1 Y 2 COL. CENTRO LA PAZ BCS.', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'}
+  ], at:[10,574]
+  formatted_text_box [
+    {text:' Telefono(s): ', size:10, font:'Calibri', style:[:normal]},
+    {text:' (624)144-4725 (624)144-4725 ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'},
+    {text:' RFC: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' BDI981121TY8 ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'},
+    {text:' Email: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' carlos_albinogg@yahoo.com.mx-----', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'}
+  ], at:[10,558]
+
+#--------------SECOND BOX DATA-------------------------------------------------------------------------------------------------------
   formatted_text_box [ 
-  		{text:'Area solicitante: ', size:10, font:'Calibri', style:[:normal]},
-    	{text:' TRANSITO CANCREJOS DEL CSL Responsable: C. Carlos Albino Garcia Grijalba', size:10, font:'Calibri', color:'a5a0a0'},
-     ], at:[10,535]
+  	{text:'Dependencia: ', size:10, font:'Calibri' },
+    {text:' SECRETARIA PARTICULA MUNICIPAL ', size:10, font:'Calibri', color:'b0a8a8'},
+  ], at:[10,535]
   formatted_text_box [ 
-    	{text:'Dependencia: ', size:10, font:'Calibri' },
-    	{text:' TRANSITO CANCREJOS DEL CSL Responsable: C. Carlos Albino Garcia Grijalba', size:10, font:'Calibri', color:'a5a0a0'},
-    	#{text:'Direccion: ', size:10, font:'Calibri'},
-    	#{text:'C. XHIpiro E/Playhon y Rocallon Manzan 11 Lote 14 Residencial Las Peñalosas', size:10, font:'Calibri', color:'a5a0a0'}
-     ], at:[10,520]
+    {text:' Responsable: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' HÉCTOR ARAGÓN AGÚNDEZ ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'},
+    {text:' Folio de solicitud: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' SOL SISTEMAS', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'}
+  ], at:[10,520]
   formatted_text_box [ 
-    	{text:'Direccion: ', size:10, font:'Calibri'},
-    	{text:'C. XHIpiro E/Playhon y Rocallon Manzan 11 Lote 14 Residencial Las Peñalosas', size:10, font:'Calibri', color:'a5a0a0'}
-     ], at:[10,505]
-  # bounding_box([5,540], width:550, height:70 ) do
-  # 	move_down 6
-  # 	indent(5) do
-  # 		stroke_axis(:step_length => 50)
-  # 		text 'Area solicitante: Cerrito del Timbre C. Matamoros SN E/Lazaro C ', size:10, font:'Calibri'
-  # 		move_down 4
-  # 		text 'Dependencia: TRANSITO CANCREJOS DEL CSL Responsable: C. Carlos Albino Garcia Grijalba', size:10, font:'Calibri'
-  # 		move_down 4
-  # 		text 
-  # 		move_down 4
-  # 		text 'Telefono(s): 144-4725 144-4725 Celular:(624)144-4725 Folio Solicitud: ZFMT/CSL-110/09/09/2016', size:10, font:'Calibri' 
-  # 	end
-  # 	transparent(1) { stroke_bounds}
-  # end
-  
-  # line [],[]
-  # line [],[]
-  # line [],[]
-  # line [],[]
-  # line [],[]
-  # line [],[]
-  # bounding_box([5,400], width:550 ) do
-  # 	transparent(1) { stroke_bounds}
-  # end
-  # bounding_box([5,300], width:550 ) do
-  # 	transparent(1) { stroke_bounds}
-  # end
-  # bounding_box([5,200], width:550 ) do
-  # 	transparent(1) { stroke_bounds}
-  # end
-  # bounding_box([5,100], width:550 ) do
-  # 	transparent(1) { stroke_bounds}
-  # end
+    {text:'Dirección: ', size:10, font:'Calibri'},
+    {text:'Palacio Municipal, XIi Ayuntamiento de Los Cabos Blvar. Mijares esq. e/ Avee Manuel Doblado', size:10, font:'Calibri', color:'b0a8a8'}
+  ], at:[10,505]
+  formatted_text_box [ 
+    {text:'Telefono(s): ', size:10, font:'Calibri'},
+    {text:'(624)144-4725 (624)144-4725 ', size:10, font:'Calibri', color:'b0a8a8'},
+    {text:'Telefono(s): ', size:10, font:'Calibri'},
+    {text:'(624)144-4725', size:10, font:'Calibri', color:'b0a8a8'}
+  ], at:[10,490]
+
+#--------------THIRD BOX DATA--------------------------------------------------------------------------------------------------------
+ desc = 'ANTECEDENTES: CONSTANTE ATASCO DE HOJA, RODILLO DE ACCESO DE HOJA DETERIORADO, RUIDO EN LA UNIDAD ADF. MANTENIMINETO Y DIAGNOSTICO EN GENERAL'
+
+  formatted_text_box [ 
+    {text:'Descripción del equipo', size:10, font:'Calibri' },
+  ], at:[10,467]
+  formatted_text_box [ 
+    {text:'Marca: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' CANON ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'},
+    {text:' Modelo: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' IMAGERUNNER 2525 ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'},
+    {text:' No. activo: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' 034794', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'}
+   ], at:[10,452]
+   formatted_text_box [ 
+    {text:'No. serie: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' FRV90485 ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'},
+    {text:' Tipo: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' REPARACION ', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'},
+    {text:' Localidad: ', size:10, font:'Calibri', style:[:normal]},
+    {text:' SAN JOSE DEL CABO', size:10, font:'Calibri', style:[:normal], color:'b0a8a8'}
+   ], at:[10,437]
+   formatted_text_box [ 
+    {text:'Descripción del error', size:10, font:'Calibri' },
+  ], at:[10,422]
+  formatted_text_box [ 
+    {text:"#{desc}", size:10, font:'Calibri', style:[:bold], color:'b0a8a8' },
+  ], at:[10,407]
+
+  # formatted_text_box [ 
+  #   {text:'Dirección: ', size:10, font:'Calibri'},
+  #   {text:'Palacio Municipal, XIi Ayuntamiento de Los Cabos Blvar. Mijares esq. e/ Avee Manuel Doblado', size:10, font:'Calibri', color:'b0a8a8'}
+  # ], at:[10,505]
+  # formatted_text_box [ 
+  #   {text:'Telefono(s): ', size:10, font:'Calibri'},
+  #   {text:'(624)144-4725 (624)144-4725 ', size:10, font:'Calibri', color:'b0a8a8'},
+  #   {text:'Telefono(s): ', size:10, font:'Calibri'},
+  #   {text:'(624)144-4725', size:10, font:'Calibri', color:'b0a8a8'}
+  # ], at:[10,490]
+
+#--------------FOURTH BOX DATA-------------------------------------------------------------------------------------------------------
+  formatted_text_box [ 
+    {text:'Materiales y/o piezas: ', size:10, font:'Calibri' },
+  ], at:[10,360]
+  formatted_text_box [ 
+    {text:'Hora entrada: ', size:10, font:'Calibri' },
+  ], at:[10,311]
+  formatted_text_box [ 
+    {text:'Fecha visita: ', size:10, font:'Calibri' },
+  ], at:[200,311]
+  formatted_text_box [ 
+    {text:'Hora salida: ', size:10, font:'Calibri' },
+  ], at:[420,311]
+
 end
