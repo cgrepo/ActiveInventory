277cd9cf83401347c009d16b1287991643ee0c0b
polken <genesis62@hotmail.com>
2017-05-11 19:47:17 +0000

    Validation of Brand NAmes

--- app/models/brand_model.rb
+++ app/models/brand_model.rb
@@ -1,6 +1,7 @@
 class BrandModel < ActiveRecord::Base
 
 	has_one :User
+  validate :brand_names
   
   def brandx=(data)
     write_attribute(:brandx, data.to_s.upcase)
@@ -10,4 +11,9 @@ class BrandModel < ActiveRecord::Base
     write_attribute(:modelx, data.to_s.upcase)
   end
   
+  def brand_names
+    unless self.brandx.match(/ACER|ASUS|CLON|COMPAQ|DELL|HP|IBM|LENOVO|SAMSUNG|SONY|ISB SOLABASIC|CANON|KONICA|XEROX|PANASONIC|CISCO|VIEWSONIC/)
+      errors.add(:brandx, "Nombre de la Marca es Incorrecto")
+    end
+  end
 end
