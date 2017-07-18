d67cb76725be6763a277a04b21dba5df50abdacd
polken <genesis62@hotmail.com>
2017-07-18 20:54:38 +0000

    2-add Scanner reference to ServiceRequest test on heroku hope it works

--- db/migrate/20170718204445_add_scanner_to_service_request.rb
+++ db/migrate/20170718204445_add_scanner_to_service_request.rb
@@ -1,5 +1,7 @@
 class AddScannerToServiceRequest < ActiveRecord::Migration
   def change
+    #add_reference :service_requests, :Scanner, index: true, foreign_key: true
     add_reference :service_requests, :Scanner, index: true, foreign_key: true
+    add_foreign_key :service_requests, :Scanners
   end
 end
