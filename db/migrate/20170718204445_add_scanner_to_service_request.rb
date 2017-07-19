2df33d9884526fc21f20274529768b6447b9f72d
polken <genesis62@hotmail.com>
2017-07-19 16:29:57 +0000

    5 adding first the foreing key with column parameter addition

--- db/migrate/20170718204445_add_scanner_to_service_request.rb
+++ db/migrate/20170718204445_add_scanner_to_service_request.rb
@@ -1,7 +1,7 @@
 class AddScannerToServiceRequest < ActiveRecord::Migration
   def change
     #add_reference :service_requests, :Scanner, index: true, foreign_key: true
-    add_reference :service_requests, :Scanner, index: true
-    add_foreign_key :service_requests, :Scanners
+    #add_reference :service_requests, :Scanner, index: true
+    add_foreign_key :service_requests, :Scanners, column: :Scanner_id
   end
 end
