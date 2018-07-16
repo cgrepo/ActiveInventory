df2d3adde41a8000f783f734cda4cca26ff155ba
polken <genesis62@hotmail.com>
2018-07-16 03:38:40 +0000

    disable carrier wave to upload

--- app/models/broken.rb
+++ app/models/broken.rb
@@ -1,7 +1,7 @@
 class Broken < ActiveRecord::Base
-    mount_uploader :picOf, MyPictureUploader
-	mount_uploader :picSerie, MyPictureUploader
-	mount_uploader :pic, MyPictureUploader
+    #mount_uploader :picOf, MyPictureUploader
+	#mount_uploader :picSerie, MyPictureUploader
+	#mount_uploader :pic, MyPictureUploader
 	belongs_to :Delegation
 	belongs_to :Dependency
 	belongs_to :user
