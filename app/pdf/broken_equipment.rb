e160283005afef7bbbe60313705350ad1d6efdd6
polken <genesis62@hotmail.com>
2018-07-16 05:03:43 +0000

    testing the pdf repo

--- app/pdf/broken_equipment.rb
+++ app/pdf/broken_equipment.rb
@@ -5,11 +5,13 @@ class BrokenEquipment < Prawn::Document
         #stroke_axis
         @brokens = brokens
         Prawn::Font::AFM.hide_m17n_warning = true
-        printRepo
+        #printRepo
+        headers
+        printRepo2
     end
     
-    def printRepo
-        imgshield = "#{Rails.root.to_s}/app/assets/images/logodire.png"	
+    def headers
+        imgshield="#{Rails.root.to_s}/app/assets/images/logodire.png"	
 		image imgshield, at: [10,710], fit:[82,82]
 		font_families["Verdana"] = { normal:{file:"#{Rails.root.to_s}/app/assets/fonts/Verdana.ttf", font:"Verdana"} }
 		font_families["Calibri"] = { normal:{file:"#{Rails.root.to_s}/app/assets/fonts/Calibri.ttf", font:"Calibri"} }
@@ -17,6 +19,25 @@ class BrokenEquipment < Prawn::Document
 			  {:text => "COORDINACION MUNICIPAL DE INFORMATICA Y SISTEMAS" , size:14, style:[:bold], font:"Verdana", color:'000000'  }, ], at:[96,690], width:500, height:100
 		formatted_text_box [ 
 			  {:text => "EQUIPO PARA BAJA" , size:22, style:[:bold], font:"Verdana", color:'747373'  }, ], at:[180,660], width:420, height:300
+    end
+    
+    def printRepo2
+        @brokens.each do |broken|
+            eqInfo = getEquipmentData(broken)
+            data = [['No OFICIAL','No SERIE','DEPENDENCIA','DESCRIPCION','MODELO']]
+            table( data,header:true, :position=> :center,:cell_style=>{size:7, :padding=>[64,0,0,90]}) do
+                cells.padding = 12
+                #cells.borders = [:left, :right]
+                cells.borders = []
+                row(0).borders = [:bottom,:top]
+                row(0).border_width = 0.5
+                row(0).font_style = :bold
+                row(0).background_color = 'f0f0f0'
+            end
+        end
+    end
+    
+    def printRepo
         move_down 110
         data = [['No OFICIAL','No SERIE','DEPENDENCIA','DESCRIPCION','MODELO']]
         @brokens.each do |broken|
@@ -56,6 +77,7 @@ class BrokenEquipment < Prawn::Document
                 row(0).background_color = 'f0f0f0'
         end
     end
+    
     private
     
     def getEquipmentData(brokenEquipment)
